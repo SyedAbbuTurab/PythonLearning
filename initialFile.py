d224a99d332880263d3d337d8efc82494e7cc622
Abbu Turab <syedabbuturab786@gmail.com>
2024-01-05 19:58:10 -0600

    Learned basics, variable types, comments

---
+++ initialFile.py
@@ -0,0 +1,32 @@
+# Variables
+''' 
+--> Variables are created when you assign a value to it 
+--> Remember that variable names are case-sensitive
+'''
+a = 5
+b = 10.5
+c = True
+myText = "Here is the text"
+
+
+print("The value of a = ", a,
+      "The value of b = ", b,
+      "The value of c = ", c,
+      "The value of Text = ", myText)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(myText))
+
+#Type casting
+d = str(3)
+D = 3
+f = float(4)
+
+print(type(d))
+print(d)
+print(D)
+print(type(f))
+
+    
---
+++ variables.py
@@ -0,0 +1,13 @@
+# Unpack a Collection
+Cars = ["Lambo", "Ferrari", "BMW"]
+a, b, c = Cars
+print(a)
+
+# Unpacking a Tuple
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
